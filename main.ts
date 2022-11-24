cf238b69fbb21a7638994a2569fe2b8de516f589
adyze <115144204+adamurbanek@users.noreply.github.com>
2022-11-24 11:34:02 +0100

    cisla

--- main.ts
+++ main.ts
@@ -1,26 +1,26 @@
-    let cislo = 0;
+let cislo = 0;
 
-    basic.forever(function() {
-        if (input.buttonIsPressed(Button.A)) {
-            cislo -= 1;
-        }
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        cislo -= 1;
+    }
 
-        if (input.buttonIsPressed(Button.B)) {
-            cislo += 1;
-        }
+    if (input.buttonIsPressed(Button.B)) {
+        cislo += 1;
+    }
 
-        if (cislo > 10) {
-           return; 
-        }
+    if (cislo > 10) {
+        return;
+    }
 
-        if (cislo < 0) {
-            return;
-        }
+    if (cislo < 0) {
+        return;
+    }
 
-        whaleysans.showNumber(cislo)
-        basic.pause(150);
-    })
-    
+    whaleysans.showNumber(cislo)
+    basic.pause(200);
+})
+
     // input.onButtonPressed(Button.A, function() {
     //     if (cislo > 0) {
     //         cislo -= 1;
